8620cb1a2efbdf23532c0686555d89d6330f459f
federpmatc <61851250+federpmatc@users.noreply.github.com>
2021-11-15 16:21:41 -0600

    Update Capstone Setup.ps1
    Replace 112 with 154

--- Capstone Scripts/Capstone Setup.ps1
+++ Capstone Scripts/Capstone Setup.ps1
@@ -1,5 +1,5 @@
 #Capstone Setup.ps1
-#1/24/2019
+#12/24/2021
 
 #region - Config IP and Computer Name
 Get-ChildItem env:
@@ -11,8 +11,6 @@ Get-NetAdapter -Physical
 
 #Remove IP address
 $interface = Get-NetAdapter -Physical | Get-NetIPInterface -AddressFamily "IPv4"
-#if DHCP is enabled there's nothing to do
-#If DHCP is disabled (static IP address), remove the default gateway, remove DNS and then enable DHCP, which will delete static IP
 If ($interface.Dhcp -eq "Disabled") {
  # Remove existing gateway
  If (($interface | Get-NetIPConfiguration).Ipv4DefaultGateway) { $interface | Remove-NetRoute -Confirm:$false }
@@ -43,8 +41,8 @@ Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDeployment
 
 #Configure this server as the first Active Directory domain controller in a new forest.
-#The new domain name is "ITNET-112.pri". This is also the name of the new forest.
-#The NetBIOS name of the domain: ITNET-112
+#The new domain name is "ITNET-154.pri". This is also the name of the new forest.
+#The NetBIOS name of the domain: ITNET-154
 #Forest Functional Level: Windows Server 2016
 #Domain Functional Level: Windows Server 2016
 #Additional Options:
@@ -63,8 +61,8 @@ Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
 -DomainMode "WinThreshold" `
--DomainName "ITNET-112.pri" `
--DomainNetbiosName "ITNET-112" `
+-DomainName "ITNET-154.pri" `
+-DomainNetbiosName "ITNET-154" `
 -ForestMode "WinThreshold" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
@@ -82,7 +80,7 @@ Get-ADGroup -Filter * | Select-Object name, groupscope
 
 #region - config DHCP
 Add-WindowsFeature -IncludeManagementTools dhcp
-#Add local DCHP groups “DHCP Administrators” and “DHCP Users” 
+#Add local DCHP groups ìDHCP Administratorsî and ìDHCP Usersî 
 #https://blogs.technet.microsoft.com/craigf/2013/06/23/installing-dhcp-on-windows-server-2012-did-not-create-the-local-groups/
 netsh dhcp add securitygroups
 
@@ -91,14 +89,14 @@ Add-DhcpServerInDC
 
 #Remove notification
 Set-ItemProperty `
-        –Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 `
-        –Name ConfigurationState `
-        –Value 2
+        ñPath registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 `
+        ñName ConfigurationState `
+        ñValue 2
 
 #############################
 ##Create a DHCP scope for the 192.168.20.0 subnet called Main Scope w/ a range of 192.168.20.200-.250
     Add-DhcpServerv4Scope `
-        -Name “192.168.20.0” `
+        -Name ì192.168.20.0î `
         -StartRange 192.168.20.200 `
         -EndRange 192.168.20.250 `
         -SubnetMask 255.255.255.0 `
@@ -109,9 +107,9 @@ Set-ItemProperty `
     ##Set DHCP Scope Options including DNSserver, DnsDomain, and Router (aka Default Gateway) used by your clients
     Set-DhcpServerv4OptionValue  `
         -ScopeId 192.168.20.0 `
-        -ComputerName DC1.ITNET-112.pri `
+        -ComputerName DC1.ITNET-154.pri `
         -DnsServer 192.168.20.101 `
-        -DnsDomain itnet-112.pri `
+        -DnsDomain itnet-154.pri `
         -Router 192.168.20.1 `
         -Verbose
 
@@ -122,12 +120,12 @@ Set-ItemProperty `
 
 #region - Configure DNS Records
     Get-DnsServerZone -ComputerName DC1
-    Get-DnsServerResourceRecord -ZoneName ITNET-112.pri
+    Get-DnsServerResourceRecord -ZoneName ITNET-154.pri
 
     #Add A Record
-    Add-DnsServerResourceRecordA -Name www -ZoneName ITNET-112.pri -IPv4Address 192.168.20.101
-    Get-DnsServerZone -Name ITNET-112.pri 
-    Get-DnsServerResourceRecord -ZoneName ITNET-112.pri
+    Add-DnsServerResourceRecordA -Name www -ZoneName ITNET-154.pri -IPv4Address 192.168.20.101
+    Get-DnsServerZone -Name ITNET-154.pri 
+    Get-DnsServerResourceRecord -ZoneName ITNET-154.pri
    
 #endregion - Configure DNS 
 
@@ -142,20 +140,20 @@ Get-NetIPConfiguration
     Tzutil.exe /s "Central Standard Time"
     Restart-computer -Force 
     
-    $Domaincred = Get-Credential -Credential "ITNET-112\administrator"  #Domain Credentials
-    Add-Computer -DomainName ITNET-112.pri -credential $Domaincred -Restart
+    $Domaincred = Get-Credential -Credential "ITNET-154\administrator"  #Domain Credentials
+    Add-Computer -DomainName ITNET-154.pri -credential $Domaincred -Restart
 
 #Verify Remote System is Domain Joined and in DNS
-    Get-DnsServerResourceRecord -ZoneName ITNET-112.pri
+    Get-DnsServerResourceRecord -ZoneName ITNET-154.pri
     Get-ADComputer -Filter *
-    Test-NetConnection Client1.ITNET-112.pri #may fail due if firewall enabled, name resolution works
+    Test-NetConnection Client1.ITNET-154.pri #may fail due if firewall enabled, name resolution works
     Set-NetFirewallProfile -Name domain -Enabled False
 #endregion - Configure Windows 10 Client
 
 #region - Verify Remote System is Domain Joined
-    Get-DnsServerResourceRecord -ZoneName ITNET-112.pri
+    Get-DnsServerResourceRecord -ZoneName ITNET-154.pri
     Get-ADComputer -Filter *
-    Test-NetConnection Client1.ITNET-112.pri #Will fail due to firewall, successful on name resolution
+    Test-NetConnection Client1.ITNET-154.pri #Will fail due to firewall, successful on name resolution
 #endregion - Verify Remote System is Domain Joined
 
 #region - Configure Member Server
@@ -197,9 +195,9 @@ Get-NetIPConfiguration
         $env:computername
    
 #Domain Join ServerB
-    $Domaincred = Get-Credential -Credential "ITNET-112\administrator"  #Domain Credentials
+    $Domaincred = Get-Credential -Credential "ITNET-154\administrator"  #Domain Credentials
     Invoke-command -ComputerName 192.168.20.252 -Credential "Administrator" -scriptblock {
-        Add-Computer -DomainName ITNET-112.pri -credential $using:Domaincred -Restart}
+        Add-Computer -DomainName ITNET-154.pri -credential $using:Domaincred -Restart}
 
     #if you enable PSR Remoting on the client, the following ilustrates that multiple commands can be executed simultaneously
     ICM -ComputerName dc2, client1 -ScriptBlock { 
@@ -208,9 +206,9 @@ Get-NetIPConfiguration
 
 #Verify Remote System is Domain Joined and in DNS
 
-    Get-DnsServerResourceRecord -ZoneName ITNET-112.pri
+    Get-DnsServerResourceRecord -ZoneName ITNET-154.pri
     Get-ADComputer -Filter *
-    Test-NetConnection DC2.ITNET-112.pri #Will fail due to firewall, successful on name resolution
+    Test-NetConnection DC2.ITNET-154.pri #Will fail due to firewall, successful on name resolution
 
 #endregion - Configure Member Server
 
@@ -220,7 +218,7 @@ Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 #This example shows how to create a credential object that is identical to the object that Get-Credential returns without prompting the user. 
 #This method requires a plain text password, which might violate the security standards in some enterprises.
 $Pword = ConvertTo-SecureString "Password01" -AsPlainText -Force
-$User = "ITNET-112\Administrator"
+$User = "ITNET-154\Administrator"
 $DomainCred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 
 #Used wizard to generate this.  i added the $pword & $domaincred
@@ -230,7 +228,7 @@ Install-ADDSDomainController `
 -Credential $DomainCred `
 -CriticalReplicationOnly:$false `
 -DatabasePath "C:\Windows\NTDS" `
--DomainName "ITNET-112.pri" `
+-DomainName "ITNET-154.pri" `
 -InstallDns:$false `
 -LogPath "C:\Windows\NTDS" `
 -NoRebootOnCompletion:$false `
@@ -241,18 +239,18 @@ Install-ADDSDomainController `
 #endregion
 
 #region - Create OUs
-New-ADOrganizationalUnit -Name TempEmployees -Path "DC=ITNET-112, DC=pri"
+New-ADOrganizationalUnit -Name TempEmployees -Path "DC=ITNET-154, DC=pri"
 #Create the Employees, Workstations, and Member Servers OUs (and sub OUs) 
 
-New-ADOrganizationalUnit -Name Employees -Path "DC=ITNET-112, DC=pri"
-New-ADOrganizationalUnit -Name Workstations -Path "DC=ITNET-112, DC=pri"
-New-ADOrganizationalUnit -Name "Member Servers" -Path "DC=ITNET-112, DC=pri"
+New-ADOrganizationalUnit -Name Employees -Path "DC=ITNET-154, DC=pri"
+New-ADOrganizationalUnit -Name Workstations -Path "DC=ITNET-154, DC=pri"
+New-ADOrganizationalUnit -Name "Member Servers" -Path "DC=ITNET-154, DC=pri"
 
-New-ADOrganizationalUnit -Name Office -Path "OU=Employees, DC=ITNET-112, DC=pri"
-New-ADOrganizationalUnit -Name Factory -Path "OU=Employees, DC=ITNET-112, DC=pri"
+New-ADOrganizationalUnit -Name Office -Path "OU=Employees, DC=ITNET-154, DC=pri"
+New-ADOrganizationalUnit -Name Factory -Path "OU=Employees, DC=ITNET-154, DC=pri"
 
-New-ADOrganizationalUnit -Name Office -Path "OU=Workstations, DC=ITNET-112, DC=pri"
-New-ADOrganizationalUnit -Name Factory -Path "OU=Workstations, DC=ITNET-112, DC=pri"
+New-ADOrganizationalUnit -Name Office -Path "OU=Workstations, DC=ITNET-154, DC=pri"
+New-ADOrganizationalUnit -Name Factory -Path "OU=Workstations, DC=ITNET-154, DC=pri"
 #endregion - Create OUs
 
 #region - Create User Accounts
@@ -263,23 +261,23 @@ New-ADUser `
 -AccountPassword (ConvertTo-SecureString "Password01" -AsPlainText -Force) `
 -Name "Admin1" `
 -Enabled $true `
--Path "CN=Users, DC=ITNET-112, DC=pri" `
+-Path "CN=Users, DC=ITNET-154, DC=pri" `
 -SamAccountName Admin1 `
--UserPrincipalName ("Admin1@ITNET-112.pri")
+-UserPrincipalName ("Admin1@ITNET-154.pri")
 
 New-ADUser `
 -AccountPassword (ConvertTo-SecureString "Password01" -AsPlainText -Force) `
 -Name "Admin2" `
 -Enabled $true `
--Path "CN=Users, DC=ITNET-112, DC=pri" `
+-Path "CN=Users, DC=ITNET-154, DC=pri" `
 -SamAccountName Admin2 `
--UserPrincipalName ("Admin2@ITNET-112.pri")
+-UserPrincipalName ("Admin2@ITNET-154.pri")
 
 #Add Admin1 & Admin2 to Admin Groups
 Add-ADGroupMember -Identity 'Domain Admins' -Members 'Admin1','Admin2'
 
-Rename-ADObject -Identity "CN=Administrator,CN=Users,DC=ITNET-112,DC=pri" -NewName "Enterprise_Admin"
+Rename-ADObject -Identity "CN=Administrator,CN=Users,DC=ITNET-154,DC=pri" -NewName "Enterprise_Admin"
 Get-ADUser -Filter "name -like 'Enterprise_Admin'"
-Get-ADUser -Filter "name -like 'Enterprise_Admin'" | Set-ADUser -UserPrincipalName "Enterprise_Admin@ITNET-112.pri" -SamAccountName "Enterprise_Admin"
+Get-ADUser -Filter "name -like 'Enterprise_Admin'" | Set-ADUser -UserPrincipalName "Enterprise_Admin@ITNET-154.pri" -SamAccountName "Enterprise_Admin"
 
 #endregion - Create User Accounts
